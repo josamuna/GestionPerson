f0d7febb7ba8945b5bb59e4dc43441e0e376493e
Josue Isamuna Nkembo <josamuna2009@gmail.com>
2025-09-03 03:51:46 +0200

    Start Testing with JUnit

---
+++ src/com/josamuna/project/tests/MySQLPersonDAO.java
@@ -0,0 +1,19 @@
+package com.josamuna.project.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MySQLPersonDAO {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
